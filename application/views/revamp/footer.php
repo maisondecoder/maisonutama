cd26a47e2b64853b3a5fecb549c16b1a5843c15a
maisondecoder <101738801+maisondecoder@users.noreply.github.com>
2024-10-13 23:30:04 +0700

    Update footer.php

--- application/views/revamp/footer.php
+++ application/views/revamp/footer.php
@@ -2,37 +2,46 @@
     <footer class="bg-dark text-light">
         <div class="container p-4">
             <div class="row">
-                <div class="col-sm-12 col-md-6">
+                <div class="col-sm-12 col-md-7">
                     <img class="mb-4" src="<?= $GLOBALS['domain_static'] . '/assets/logo-maison-navbar-putih.webp' ?>" alt="Maison Living" height="40">
                 </div>
-                <div class="col-sm-12 col-md-3">
-                    <h4>Our Stores</h4>
-                    <p class="fs-6 ">Monday-Sunday<br>
-                        10 AM - 8 PM</p>
-                    <p class="fs-6"><strong>Kemang Jakarta</strong><br>
-                        Jl. Raya Kemang Selatan No.31<br>
-                        Jakarta Selatan, 12730<br>
-                        <a href="https://goo.gl/maps/YogV1V2a7HVtrFC97" target="_blank" class="text-reset">Open Google Maps <i class="fa-solid fa-arrow-up-right-from-square"></i></a>
-                    </p>
-                    <p class="fs-6 mb-4"><strong>BSD Alam Sutera</strong><br>
-                        Jl. Raya Serpong Km 8 No. 7<br>
-                        Tangerang Selatan, 15326<br>
-                        <a href="https://goo.gl/maps/6ZeBuiVJioZoCq7r5" target="_blank" class="text-reset">Open Google Maps <i class="fa-solid fa-arrow-up-right-from-square"></i></a>
-                    </p>
-                </div>
-                <div class="col-sm-12 col-md-3">
-                    <h4>Contact Us</h4>
-                    <p class="fs-6"><strong>Kemang Jakarta</strong><br>
-                        Whatsapp : 0813-8818-0020<br>
-                        Phone : 021-2272-0220
-                    </p>
-                    <p class="fs-6"><strong>BSD Alam Sutera</strong><br>
-                        Whatsapp : 0819-3000-2525<br>
-                        Phone : 021-5569-5924
-                    </p>
+                <div class="col-sm-12 col-md-5">
+                    <div class="row mb-4">
+                        <h4>Contact Us</h4>
+                    </div>
+                    <div class="row mb-4">
+                        <div class="col-sm-6 col-md-6 mb-4">
+                            <h5>Kemang Jakarta</h5>
+                            <p>
+                                Jl. Raya Kemang Selatan No.31 
+                                Jakarta Selatan, 12730<br>
+                            </p>
+                            <div>
+                                WA : 0813-8818-0020<br>
+                                Phone : 021-2272-0220<br>
+                                <a href="https://goo.gl/maps/YogV1V2a7HVtrFC97" target="_blank" class="text-reset">Open Google Maps <i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-6 mb-4">
+                            <h5>BSD Alam Sutera</h5>
+                            <p>
+                                Jl. Raya Serpong Km 8 No. 7 
+                                Tangerang Selatan, 15326<br>
+                            <div>
+                                WA : 0819-3000-2525<br>
+                                Phone : 021-5569-5924<br>
+                                <a href="https://goo.gl/maps/6ZeBuiVJioZoCq7r5" target="_blank" class="text-reset">Open Google Maps <i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mb-4">
+                        <div class="col-12">Opening Hours: Monday-Sunday, 10 AM - 8 PM</div>
+                    </div>
                 </div>
             </div>
             <div class="text-center p-4">&copy;<?= date("Y"); ?>. PT Maison Cipta Kreasindo</div>
+        </div>
+
         </div>
     </footer>
     <!-- Footer -->
