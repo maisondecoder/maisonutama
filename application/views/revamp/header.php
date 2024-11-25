7e6d88fb8f181e557b3a5b68269706cb638ffdd4
maisondecoder <101738801+maisondecoder@users.noreply.github.com>
2024-11-25 11:50:39 +0700

    update pixel pakarjasa

--- application/views/revamp/header.php
+++ application/views/revamp/header.php
@@ -187,12 +187,37 @@
     <?php
     if (isset($_GET['code'])) {
         if ($_GET['code'] == 'pkj') {
-
-            //MASUKAN PIXEL/TRACKER MARKETER DI SINI
-
+    ?>
+            <!-- Meta Pixel Code -->
+            <script>
+                ! function(f, b, e, v, n, t, s) {
+                    if (f.fbq) return;
+                    n = f.fbq = function() {
+                        n.callMethod ?
+                            n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+                    };
+                    if (!f._fbq) f._fbq = n;
+                    n.push = n;
+                    n.loaded = !0;
+                    n.version = '2.0';
+                    n.queue = [];
+                    t = b.createElement(e);
+                    t.async = !0;
+                    t.src = v;
+                    s = b.getElementsByTagName(e)[0];
+                    s.parentNode.insertBefore(t, s)
+                }(window, document, 'script',
+                    'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '3518214025150823');
+                fbq('track', 'PageView');
+            </script>
+            <noscript><img height="1" width="1" style="display:none"
+                    src="https://www.facebook.com/tr?id=3518214025150823&ev=PageView&noscript=1" /></noscript>
+            <!-- End Meta Pixel Code -->
+        <?php
         }
     } else {
-    ?>
+        ?>
         <!--- PIXEL HIPROS ADA 3 TRACKER -->
         <!-- Meta Pixel Code -->
         <script>
