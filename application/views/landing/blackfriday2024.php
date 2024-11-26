97e104ee34c001cac3867527799a37819dde2168
maisondecoder <101738801+maisondecoder@users.noreply.github.com>
2024-11-27 05:13:50 +0700

    Update blackfriday2024.php

--- application/views/landing/blackfriday2024.php
+++ application/views/landing/blackfriday2024.php
@@ -9,7 +9,7 @@
             foreach ($promo_products as $key => $product) { ?>
                 <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 mb-2">
                     <a class="text-decoration-none" href="<?= base_url("our-collections/") . $product['product_slug'] . '?via=blackfriday2024'; ?>">
-                        <img class="img-fluid mb-2 border topacity from-popupbanner" style="width:300px !important; height:200px !important; object-fit:cover !important;" src="<?= $GLOBALS['domain_static'] . '/assets/products/thumbnail/' . $product['product_thumbnail']; ?>" alt="" width="300" height="200">
+                        <img class="img-fluid mb-2 border topacity item-from-banner" style="width:300px !important; height:200px !important; object-fit:cover !important;" src="<?= $GLOBALS['domain_static'] . '/assets/products/thumbnail/' . $product['product_thumbnail']; ?>" alt="" width="300" height="200">
                         <h4 class="text-secondary"><?= $product['product_name']; ?></h4>
                     </a>
                 </div>
